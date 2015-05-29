8cec84e3623d18fede4b5b83a1c45d7d7846ed67
hoogw <huguowen@gmail.com>
2015-05-29 16:34:01 -0700

    verson 1.0 launch

--- geoJSON_to_iosJSON_converter/Form1.Designer.cs
+++ geoJSON_to_iosJSON_converter/Form1.Designer.cs
@@ -74,9 +74,9 @@
             // 
             this.button2.Location = new System.Drawing.Point(15, 81);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(102, 23);
+            this.button2.Size = new System.Drawing.Size(59, 23);
             this.button2.TabIndex = 4;
-            this.button2.Text = "Convert file";
+            this.button2.Text = "Zoning";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
---
+++ geoJSON_to_iosJSON_converter/Model/geoJSON.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace geoJSON_to_iosJSON_converter.Model
+{
+   
+
+    public class Properties
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+
+    public class Geometry
+    {
+        public string type { get; set; }
+        public List<List<double>> coordinates { get; set; }
+    }
+
+    public class geoJSON
+    {
+        public string type { get; set; }
+        public Properties properties { get; set; }
+        public Geometry geometry { get; set; }
+    }
+
+    
+
+
+    public class LatLn
+    {
+        public double lat { get; set; }
+        public double ln { get; set; }
+    }
+
+
+    public class iosJSON
+    {
+        public string name { get; set; }
+        public string zone { get; set; }
+        public string apn { get; set; }
+        public string address { get; set; }
+        
+        public List<LatLn> feature { get; set; }
+    
+    }
+
+
+
+
+
+
+}
